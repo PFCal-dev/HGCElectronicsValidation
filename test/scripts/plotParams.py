c7fd5d38ba9bb2d4a3ecfdd2969c23b0cccbf250
Pedro <psilva@cern.ch>
2019-10-10 18:10:47 +0200

    updating

--- test/scripts/plotParams.py
+++ test/scripts/plotParams.py
@@ -4,6 +4,7 @@ from drawRadiationMapPlots import drawHeader
 
 ROOT.gStyle.SetOptStat(0)
 ROOT.gStyle.SetOptTitle(0)
+ROOT.gROOT.SetBatch(True)
 
 qe2fc=1.60217646E-4;
 ileakParams = [0.993,-42.668]
@@ -22,6 +23,8 @@ SENSORDATA=[('dd-FZ 120#mum', [1.5e+15, -3.00394e-17, 0.318083],  120.*67.,  50,
             #('epi 200#mum',   [1.5e+15, -3.09878e-16, 0.211207],  200.*70.,  65, 1.18*(300.e-4), 2, ROOT.kAzure-2),
             #('epi 300#mum',   [6e+14,   -7.96539e-16, 0.251751],  300.*73.,  45, 1.18*(300.e-4), 2, ROOT.kRed+1),
     ]
+noiseColors=[ROOT.kBlack,ROOT.kAzure-2,ROOT.kRed+1,ROOT.kBlack]
+noiseLines=[1,1,1,2]
 
 for title,params,mipeq,cap,vol,ls,ci in SENSORDATA:
 
@@ -44,15 +47,13 @@ for title,params,mipeq,cap,vol,ls,ci in SENSORDATA:
     ileak[-1].SetLineWidth(2)
     ileak[-1].SetLineColor(ci)
 
-
-    continue
-
+    icce=len(cces)
     signal.append( cces[-1].Clone('sig%d'%icce) )
     signal[-1].SetParameter(3,mipeq*qe2fc)
     
     for key in noise_params: 
         noise[key].append( ROOT.TF1('noiseq%d%d'%(key,icce),
-                                    '%f*sqrt(1.25)*sqrt( pow(840.*sqrt(exp([0]*log(x)+[1])*[2]),2)+pow([3]+[4]*[6]+[5]*pow([6],2),2) )'%qe2fc,
+                                    '%f*sqrt( pow(840.*sqrt(exp([0]*log(x)+[1])*[2]),2)+1.25*pow([3]+[4]*[6]+[5]*pow([6],2),2) )'%qe2fc,
                                     1e13,1e17) )
         for ip in range(len(ileakParams)):
             noise[key][-1].SetParameter(ip,ileakParams[ip])
@@ -61,6 +62,10 @@ for title,params,mipeq,cap,vol,ls,ci in SENSORDATA:
             noise[key][-1].SetParameter(ip+1+len(ileakParams),noise_params[key][ip])
         noise[key][-1].SetParameter(1+len(ileakParams)+len(noise_params[key]),cap)
         noise[key][-1].SetTitle(title)
+        noise[key][-1].SetLineWidth(2)
+        noise[key][-1].SetLineColor(noiseColors[len(noise[key])-1])
+        noise[key][-1].SetLineStyle(noiseLines[len(noise[key])-1])
+
 
 
 c=ROOT.TCanvas('c','c',600,600)
@@ -79,23 +84,23 @@ frame.GetXaxis().SetTitleOffset(1.3)
 
 for gr,title,name,yran in [(cces,'CCE','CCE',(0,1)),
                            (ileak,'I_{leak} [#muA]','Ileak',(1e-1,100)),
-                           #(signal,'Signal [fC]','Signal',(0,5)),                           
-                           #(noise[80],'Noise (80fC) [fC]','Noise80',(0,5)),
-                           #(noise[160],'Noise (160fC) [fC]','Noise160',(0,5)),
-                           #(noise[320],'Noise (320fC) [fC]','Noise320',(0,5))
+                           (signal,'Signal [fC]','Signal',(0,5)),                           
+                           (noise[80],'Noise (80fC) [fC]','Noise80',(0,5)),
+                           (noise[160],'Noise (160fC) [fC]','Noise160',(0,5)),
+                           (noise[320],'Noise (320fC) [fC]','Noise320',(0,5))
                        ]:
     
-    if name=='Ileak':
+    c.SetLogy(False)
+    if name=='Ileak' or 'Noise' in name or 'Signal' in name:
         leg=ROOT.TLegend(0.15,0.67,0.4,0.95)
-        c.SetLogy()
+        if name=='Ileak': 
+            c.SetLogy()
     else:
         leg=ROOT.TLegend(0.15,0.12,0.4,0.4)
-        c.SetLogy(False)
 
     frame.GetYaxis().SetTitle(title)
     frame.GetYaxis().SetRangeUser(yran[0],yran[1])
     frame.Draw()
-
 
 
     leg.SetBorderSize(0)
