6950b5456cb81594d3babdaab3f00d828b1cfa1a
Pedro <psilva@cern.ch>
2021-09-23 09:06:12 +0200

    updtaing 113x

--- test/produceForOptim.sh
+++ test/produceForOptim.sh
@@ -5,13 +5,21 @@ scenarios=(
     3iab_120-600V_200-300-600V
     4iab_120-600V_200-300-800V
 )
+
+dosemap=doseParams_3000fb_fluka-6.2.0.1.txt
+geometry=Extended2026D86
+flatfile=geomCMSSW10052021_corrected.txt
+
+dosemap=doseParams_3000fb_fluka-3.7.20.txt
+geometry=Extended2026D49
+flatfile=geomnew_corrected_withmult_F_rotations_v11.1.txt
+
 for s in ${scenarios[@]}; do
-    baseOpts="doseMap=SimCalorimetry/HGCalSimProducers/data/doseParams_3000fb_fluka-6.2.0.1.txt"
-    baseOpts="${baseOpts} geometry=Extended2026D86"
+    baseOpts="doseMap=SimCalorimetry/HGCalSimProducers/data/${dosemap}"
+    baseOpts="${baseOpts} geometry=${geometry}"
     baseOpts="${baseOpts} savePadInfo=True"
     baseOpts="${baseOpts} scenario=${s}"
-    baseOpts="${baseOpts} uvmapfile=UserCode/HGCElectronicsValidation/data/geomCMSSW10052021_corrected.txt"
+    baseOpts="${baseOpts} uvmapfile=UserCode/HGCElectronicsValidation/data/${flatfile}"
 
-    #cmsRun test/hgcsiopscan_cfg.py ${baseOpts} wafersFromCMSSW=True output=SiOpScan_${scenarios}_cmssw_wafers.root 
-    cmsRun test/hgcsiopscan_cfg.py ${baseOpts} wafersFromCMSSW=False output=SiOpScan_${scenarios}_flattxt_wafers.root 
+    cmsRun test/hgcsiopscan_cfg.py ${baseOpts} wafersFromCMSSW=False output=SiOpScan_${s}_${geometry}.root 
 done
