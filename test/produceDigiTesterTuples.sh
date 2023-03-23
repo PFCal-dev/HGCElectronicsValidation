2d9b46ec6cfe2bb5b2385a27594eb61f9dc6b35b
Pedro <psilva@cern.ch>
2023-03-23 14:16:24 +0100

    adding notebook

--- test/produceDigiTesterTuples.sh
+++ test/produceDigiTesterTuples.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
 setups=(
-#    SinglePhotonEGun_eta1p8_13_0_0_pre4_D99_3iab,useVanillaCfg=False,byDoseAlgo=0
-#    SinglePhotonEGun_eta2p0_13_0_0_pre4_D99_3iab,useVanillaCfg=False,byDoseAlgo=0
-#    SinglePhotonEGun_eta2p5_13_0_0_pre4_D99_3iab,useVanillaCfg=False,byDoseAlgo=0
-#    SinglePhotonEGun_eta2p8_13_0_0_pre4_D99_3iab,useVanillaCfg=False,byDoseAlgo=0
-#    SingleK0LEGun_eta1p8_13_0_0_pre4_D99_3iab,useVanillaCfg=False,byDoseAlgo=0
-#    SingleK0LEGun_eta2p0_13_0_0_pre4_D99_3iab,useVanillaCfg=False,byDoseAlgo=0
-#    SingleK0LEGun_eta2p5_13_0_0_pre4_D99_3iab,useVanillaCfg=False,byDoseAlgo=0
-#    SingleK0LEGun_eta2p8_13_0_0_pre4_D99_3iab,useVanillaCfg=False,byDoseAlgo=0
+    SinglePhotonEGun_eta1p8_13_0_0_pre4_D99_3iab,useVanillaCfg=False,byDoseAlgo=0
+    SinglePhotonEGun_eta2p0_13_0_0_pre4_D99_3iab,useVanillaCfg=False,byDoseAlgo=0
+    SinglePhotonEGun_eta2p5_13_0_0_pre4_D99_3iab,useVanillaCfg=False,byDoseAlgo=0
+    SinglePhotonEGun_eta2p8_13_0_0_pre4_D99_3iab,useVanillaCfg=False,byDoseAlgo=0
+    SingleK0LEGun_eta1p8_13_0_0_pre4_D99_3iab,useVanillaCfg=False,byDoseAlgo=0
+    SingleK0LEGun_eta2p0_13_0_0_pre4_D99_3iab,useVanillaCfg=False,byDoseAlgo=0
+    SingleK0LEGun_eta2p5_13_0_0_pre4_D99_3iab,useVanillaCfg=False,byDoseAlgo=0
+    SingleK0LEGun_eta2p8_13_0_0_pre4_D99_3iab,useVanillaCfg=False,byDoseAlgo=0
     SinglePhotonEGun_eta1p8_13_0_0_pre4_D99,useVanillaCfg=True,byDoseAlgo=1
-#    SinglePhotonEGun_eta2p0_13_0_0_pre4_D99,useVanillaCfg=True,byDoseAlgo=1
-#    SinglePhotonEGun_eta2p5_13_0_0_pre4_D99,useVanillaCfg=True,byDoseAlgo=1
-#    SinglePhotonEGun_eta2p8_13_0_0_pre4_D99,useVanillaCfg=True,byDoseAlgo=1
-#    SingleK0LEGun_eta1p8_13_0_0_pre4_D99,useVanillaCfg=True,byDoseAlgo=1
-#    SingleK0LEGun_eta2p0_13_0_0_pre4_D99,useVanillaCfg=True,byDoseAlgo=1
-#    SingleK0LEGun_eta2p5_13_0_0_pre4_D99,useVanillaCfg=True,byDoseAlgo=1
-#    SingleK0LEGun_eta2p8_13_0_0_pre4_D99,useVanillaCfg=True,byDoseAlgo=1
+    SinglePhotonEGun_eta2p0_13_0_0_pre4_D99,useVanillaCfg=True,byDoseAlgo=1
+    SinglePhotonEGun_eta2p5_13_0_0_pre4_D99,useVanillaCfg=True,byDoseAlgo=1
+    SinglePhotonEGun_eta2p8_13_0_0_pre4_D99,useVanillaCfg=True,byDoseAlgo=1
+    SingleK0LEGun_eta1p8_13_0_0_pre4_D99,useVanillaCfg=True,byDoseAlgo=1
+    SingleK0LEGun_eta2p0_13_0_0_pre4_D99,useVanillaCfg=True,byDoseAlgo=1
+    SingleK0LEGun_eta2p5_13_0_0_pre4_D99,useVanillaCfg=True,byDoseAlgo=1
+    SingleK0LEGun_eta2p8_13_0_0_pre4_D99,useVanillaCfg=True,byDoseAlgo=1
 )
 
 baseDir=/eos/cms/store/cmst3/group/hgcal/CMG_studies/Production
-outDir=/eos/cms/store/cmst3/group/hgcal/CMG_studies/psilva/DigiTester
-
+outDir=/eos/cms/store/cmst3/group/hgcal/CMG_studies/psilva/DigiTester/2023Mar20
+mkdir -p ${outDir}
 for i in ${setups[@]}; do
     IFS=',' read tag useVanilla doseAlgo <<< "${i}"
     echo "*******************************************"
